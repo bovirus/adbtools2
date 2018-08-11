b2568b6358597a3e97ab9c74f16827cbca1c97ea
Valerio Di Giampietro <valerio@digiampietro.com>
2018-08-11 23:22:11 +0200

    Added the firmware modification kit for DVA-5592_A1_WI_20180405.sig firmware

---
+++ mod-kit/mod-kit-configure.sh
@@ -0,0 +1,165 @@
+#!/bin/sh
+#
+# prepare the mod-kit folders layout and does some preliminary check
+# This script has been checked ONLY with the firmware
+# DVA-5592_A1_WI_20180405.sig with other firmware version probably
+# will not function correctly.
+#
+# in case of different firmware version this script can be used as a
+# guide to do a similar job modifying what needs to be modified
+#
+# START OF CUSTOMIZABLE PARAMETERS
+# --------------------------------------------------------------------------------
+MK_BASEDIR=$HOME/mod-kit                    # base dir of the mod-kit files
+MKFS_JFFS2=/usr/local/bin/mkfs.jffs2-lzma   # mkfs.jffs2 command with lzma patch
+# --------------------------------------------------------------------------------
+# END OF CUSTOMIZABLE PARAMETERS
+#
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+#
+#
+usage() {
+    echo "usage: ./mod-kit-configure.sh <firmware file>"
+    echo "   example:"
+    echo "   ./mod-kit-configure.sh /path/to/DVA-5592_A1_WI_20180405.sig"
+    echo
+    echo "   This command will prepare the mod-kit folders layout at"
+    echo "   $MK_BASEDIR"
+}
+
+# ------ check firmware file
+check_fwfile() {
+    magic_number=`hexdump -s 0 -n 4 -ve '/1 "%c"' $1`
+    if [ "$magic_number" != "yIMG" ]
+    then
+	echo "ERROR: Wrong firmware file (wrong magic number)"
+	exit 1
+    fi
+    
+    remote_version=`hexdump -s 48 -n 16 -ve '/1 "%c"' $1`
+    if [ "$remote_version" != "DVA-5592_A1_WI_2" ]
+    then
+	echo "ERROR: Wrong version (expecting DVA-5592_A1_WI_2 got $remote_version)"
+	exit 2
+    fi
+
+    remote_date=`hexdump -s 64 -n 16 -ve '/1 "%c"' $1`
+    if [ "$remote_date" != "2018-04-11 12:42" ]
+    then
+	echo "ERROR: Wrong release date (expecting 2018-04-11 12:42 got $remote_date)"
+	exit 3
+    fi
+    echo "# ------ firmware file ok"
+}
+
+
+# ------ check input parameter (presence and firmware file existence)
+echo "# ------ check input parameter (presence and firmware file existence)"
+
+
+if [ "$1" = "" ] 
+then
+    usage
+    exit
+fi
+
+FW_PATH=$1
+
+if [ ! -e $FW_PATH ]
+then
+    echo $FW_PATH does not exists
+    exit
+fi
+FW_FILE=`basename $FW_PATH`
+
+check_fwfile $FW_PATH 
+
+# ------ check mkfs.jffs2 existence and lzma support
+echo "# ------ check mkfs.jffs2 existence and lzma support"
+
+if [ ! -x $MKFS_JFFS2 ]
+then
+    MKFS_JFFS2=`which mkfs.jffs2`
+fi
+
+$MKFS_JFFS2 -L 2>&1 | grep lzma
+ret=$?
+if [ ! "$ret" = "0" ]
+then
+    echo "$MKFS_JFFS2 does not support lzma compression"
+    echo "please follow documentation, modify this script's"
+    echo "MKFS_JFFS2 variable to point to a mkfs.jffs2 command"
+    echo "with lzma support. Follow documentation to apply"
+    echo "the lzma patch to mkfs.jffs2 source"
+    exit 1
+else
+    echo "#        mkfs.jffs2 with lzma support found"
+fi
+
+
+# ------ check for other needed commands
+echo "# ------ check for other needed commands"
+
+for i in jefferson dd fakeroot setfacl rsync patch truncate md5sum xxd
+do which $i
+   ret=$?
+   if [ ! "$ret" = "0" ]
+   then
+       echo "$i not present"
+       echo "please install it" 
+       if [ "$i" = "jefferson" ]
+       then
+	   echo "look at https://github.com/sviehb/jefferson"
+       fi  
+       exit 1
+   else
+       echo "#        $i found"
+   fi
+done
+
+
+
+# ------ print main variables and create $MK_BASEDIR/conf.sh file
+echo "# ------ main variables and create $MK_BASEDIR/conf.sh file"
+echo "         MK_BASEDIR    $MK_BASEDIR"
+echo "         MKFS_JFFS2    $MKFS_JFFS2"
+echo "         FW_FILE       $FW_FILE"
+echo "         SCRIPT        $SCRIPT"
+echo "         SCRIPTPATH    $SCRIPTPATH"
+echo "MK_BASEDIR=\"$MK_BASEDIR\""           > $SCRIPTPATH/conf.sh
+echo "MKFS_JFFS2=\"$MKFS_JFFS2\""          >> $SCRIPTPATH/conf.sh
+echo "FW_FILE=\"$FW_FILE\""                >> $SCRIPTPATH/conf.sh 
+echo
+
+# ------ prepare folder layouts
+echo "# ------ preparing folder layout at $MK_BASEDIR"
+echo "#        $MK_BASEDIR/input         original firmware file and extracted data will go here"
+echo "#        $MK_BASEDIR/input/root    original firmware root file system will go here"
+echo "#        $MK_BASEDIR/root-patch    patches to each file will go here"
+echo "#        $MK_BASEDIR/root-overlay  files here will be added/will overwrite files in the destination root"
+echo "#        $MK_BASEDIR/output        modified firmware and output images will go here"
+echo "#        $MK_BASEDIR/output/root   modified root file system will go here"
+echo
+
+for i in $MK_BASEDIR/input \
+	 $MK_BASEDIR/input/root \
+	 $MK_BASEDIR/root-patch \
+	 $MK_BASEDIR/root-overlay \
+	 $MK_BASEDIR/output \
+	 $MK_BASEDIR/output/root
+do if [ ! -d $i ]
+   then
+       echo "#    making dir $i"
+       mkdir -p $i
+   fi
+done
+
+# ------ copy files to mod-kit folder
+echo "# ------ copying $FW_PATH --> $MK_BASEDIR/input"
+cp $FW_PATH $MK_BASEDIR/input
+echo "# ------ copying patch and overlay files to $MK_BASEDIR/root-patch"
+rsync -rav $SCRIPTPATH/root-patch/   $MK_BASEDIR/root-patch/
+rsync -rav $SCRIPTPATH/root-overlay/ $MK_BASEDIR/root-overlay/
+cp $SCRIPTPATH/device-table.txt      $MK_BASEDIR/
+cp $SCRIPTPATH/root-permissions.acl  $MK_BASEDIR/
---
+++ mod-kit/mod-kit-run.sh
@@ -0,0 +1,162 @@
+#!/bin/sh
+#
+# this script source conf.sh, on same directory, and then generates an unsigned modified
+# firmware from DVA-5592_A1_WI_20180405.sig (currently works only on this firmware)
+#
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+ERASESIZE=$((128 * 1024))
+. $SCRIPTPATH/conf.sh
+FW_NEW_FILE=`echo "$FW_FILE"|sed 's/\.sig$/-mod.sig/'`
+if [ "$FW_NEW_FILE" = "$FW_FILE" ]
+then
+    FW_NEW_FILE="$FW_FILE-mod.sig"
+fi
+
+# ------ print main variables and create $MK_BASEDIR/conf.sh file
+echo "------ main variables and create $MK_BASEDIR/conf.sh file"
+echo "       MK_BASEDIR    $MK_BASEDIR"
+echo "       MKFS_JFFS2    $MKFS_JFFS2"
+echo "       FW_FILE       $FW_FILE"
+echo "       SCRIPT        $SCRIPT"
+echo "       SCRIPTPATH    $SCRIPTPATH"
+echo "       ERASESIZE     $ERASESIZE"
+echo "       FW_NEW_FILE   $FW_NEW_FILE"
+echo
+
+# ------ extract boot and root fs from firmware file
+echo "------ extract boot and root fs from firmware file"
+dd if=$MK_BASEDIR/input/$FW_FILE bs=256 skip=514 count=94720 of=$MK_BASEDIR/input/boot-root-fs.bin
+
+# ------ split boot and root partitions
+echo "# ------ split boot and root partitions"
+PSPOS=`grep --byte-offset --only-matching --text YAPS-PartitionSplit $MK_BASEDIR/input/boot-root-fs.bin|awk -F: '{print $1}'`
+SPLITPOS=$(($PSPOS+256))
+BOOTROOTSIZE=`wc -c $MK_BASEDIR/input/boot-root-fs.bin|awk '{print $1}'`
+ROOTEND=$(($BOOTROOTSIZE - $ERASESIZE))
+ROOTLEN=$(($ROOTEND - $SPLITPOS))
+echo "         SPLITPOS:     $SPLITPOS"
+echo "         PSPOS:        $PSPOS"
+echo "         BOOTROOTSIZE: $BOOTROOTSIZE"
+echo "         ROOTEND:      $ROOTEND"
+echo "         ROOTLEN:      $ROOTLEN"
+
+# ------ extract boot partition image
+echo "# ------ extract boot partition image"
+dd if=$MK_BASEDIR/input/boot-root-fs.bin of=$MK_BASEDIR/input/boot-fs.bin bs=256 count=$(($SPLITPOS / 256))
+
+# ------ extract root partition image, takes some time
+echo "# ------ extract root partition image, takes some time"
+dd if=$MK_BASEDIR/input/boot-root-fs.bin of=$MK_BASEDIR/input/root-fs.bin bs=256 skip=$(($SPLITPOS / 256)) count=$(($ROOTLEN /256 ))
+
+# ------ extract end of file system marker
+echo "# ------ extract end of file system marker"
+dd if=$MK_BASEDIR/input/boot-root-fs.bin of=$MK_BASEDIR/input/eofs.bin bs=256 skip=$(($ROOTEND / 256))
+
+# ------ extract root file system with jefferson
+echo "# ------ extract root file system with jefferson, it takes some time"
+fakeroot jefferson -f -d $MK_BASEDIR/input/root $MK_BASEDIR/input/root-fs.bin
+
+# ------ fix extracted root file system
+echo "# ------ fix extracted root file system"
+CURRWD=`pwd`
+cd $MK_BASEDIR/input/root/fs_2
+for i in `find . -maxdepth 1 -type l -print`;do mv $i ../fs_1/sbin/;done
+for i in `find . -maxdepth 1 -type f -print`;do mv $i ../fs_1/sbin/;done
+mv conf ../fs_1/www/
+mv sbin ../fs_1/usr/
+mv bin ../fs_1/usr/
+mv htdocs ../fs_1/www/
+mv lib ../fs_1/usr/
+mv nls ../fs_1/www/
+mv pages ../fs_1/www/
+mv share ../fs_1/usr/
+mv yapl ../fs_1/www/
+cd $CURRWD
+rmdir $MK_BASEDIR/input/root/fs_2
+mv $MK_BASEDIR/input/root/fs_1/[a-z]* $MK_BASEDIR/input/root/
+rmdir $MK_BASEDIR/input/root/fs_1
+cp -p $MK_BASEDIR/input/root/bin/busybox $MK_BASEDIR/input/root/sbin/init
+chmod 755 $MK_BASEDIR/input/root/sbin/init
+
+# ------ fix permissions on extracted root file system
+echo "# ------ fix permissions on extracted root file system"
+cd $MK_BASEDIR/input/root
+fakeroot setfacl --restore=$MK_BASEDIR/root-permissions.acl
+cd $CURRWD
+
+# ------ copy original root file system to new root file system
+echo "# ------ copy original root file system to new root file system"
+rsync -rav $MK_BASEDIR/input/root/ $MK_BASEDIR/output/root/
+
+# ------ apply patches to new root file system
+for i in `find $MK_BASEDIR/root-patch -name \*.patch -type f`
+do echo "          applying $i"
+   F=`echo $i|sed 's/root-patch/output\/root/'|sed 's/\.patch$//'`
+   echo "          to       $F"
+   patch $F $i
+done
+
+cd $MK_BASEDIR/output/root
+fakeroot setfacl --restore=$MK_BASEDIR/root-permissions.acl
+cd $CURRWD
+
+# ------ apply overlay to new root file system
+echo "# ------ apply overlay to new root file system"
+rsync -rav $MK_BASEDIR/root-overlay/ $MK_BASEDIR/output/root/
+
+
+
+# ------ create new root file system image, step 1 mkfs.jffs2
+echo "# ------ create new root file system image, step1 mkfs.jffs2"
+$MKFS_JFFS2  -r $MK_BASEDIR/output/root/ -s 4096 -e 131072 \
+	     -y 95:lzma -n -o $MK_BASEDIR/output/rootfs-step1.bin \
+	     -l -v -D $MK_BASEDIR/device-table.txt -U \
+	     2>&1 | tee $MK_BASEDIR/output/mkjffs2-step1.log
+
+# ------ zero pad the new root file system image to same size as original root file system 
+echo "# ------ zero pad the new root file system image to same size as original root file system"
+NEWROOTSIZE=`wc -c $MK_BASEDIR/output/rootfs-step1.bin|awk '{print $1}'`
+PADSIZE=$(($NEWROOTSIZE / $ERASESIZE))
+PADSIZE=$(($PADSIZE + 1 ))
+PADSIZE=$(($PADSIZE * 1024 * 128 ))
+PADSIZE=$(($PADSIZE - $NEWROOTSIZE))
+echo "         PADSIZE:    $PADSIZE"
+cp $MK_BASEDIR/output/rootfs-step1.bin $MK_BASEDIR/output/rootfs-step2.bin
+dd if=/dev/zero of=$MK_BASEDIR/output/rootfs-step2.bin bs=1 count=$PADSIZE seek=$NEWROOTSIZE conv=notrunc
+cat $MK_BASEDIR/output/rootfs-step2.bin $MK_BASEDIR/input/eofs.bin  > $MK_BASEDIR/output/rootfs-step3.bin
+
+NEWROOTSIZE=`wc -c $MK_BASEDIR/output/rootfs-step3.bin|awk '{print $1}'`
+
+if [ ! $ROOTLEN -ge $NEWROOTSIZE ]
+then
+    echo "UNRECOVERABLE PROBLEM: the new root image is larger the the original root image"
+    exit 1
+fi
+
+PADSIZE=$(($ROOTLEN - $NEWROOTSIZE))
+
+cp $MK_BASEDIR/output/rootfs-step3.bin $MK_BASEDIR/output/root-fs.bin
+dd if=/dev/zero of=$MK_BASEDIR/output/root-fs.bin bs=1 count=$PADSIZE seek=$NEWROOTSIZE conv=notrunc
+
+# ------ generate the new boot/root image
+echo "# ------ generate the new boot/root image"
+cat $MK_BASEDIR/input/boot-fs.bin $MK_BASEDIR/output/root-fs.bin $MK_BASEDIR/input/eofs.bin > $MK_BASEDIR/output/boot-root-fs.bin
+
+# ------ generate the new unsigned firmware image
+echo "# ------ generate the new unsigned firmware image"
+cp $MK_BASEDIR/input/$FW_FILE  $MK_BASEDIR/output/$FW_NEW_FILE
+FWNEWSIZE=`wc -c $MK_BASEDIR/output/$FW_NEW_FILE | awk '{print $1}'`
+FWNEWSIZE=$(($FWNEWSIZE - 256))
+# ------ truncate signature from unsigned firmware image
+truncate -s $FWNEWSIZE $MK_BASEDIR/output/$FW_NEW_FILE
+
+# ------ overwrite the original boot/root file system image with the modified one
+echo "# ------ overwrite the original boot/root file system image with the modified one"
+dd if=$MK_BASEDIR/output/boot-root-fs.bin of=$MK_BASEDIR/output/$FW_NEW_FILE bs=256 seek=514 count=94720 conv=notrunc
+
+# ------ recalculate checksum in the modified firmware
+echo "# ------ recalculate checksum in the modified firmware"
+dd if=/dev/zero of=$MK_BASEDIR/output/$FW_NEW_FILE conv=notrunc bs=1 count=16 seek=240
+hash=$(md5sum $MK_BASEDIR/output/$FW_NEW_FILE | cut -d " " -f1)
+echo -n $hash | xxd -r -p | dd of=$MK_BASEDIR/output/$FW_NEW_FILE conv=notrunc bs=1 count=16 seek=240
